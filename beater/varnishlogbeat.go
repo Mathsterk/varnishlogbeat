280a23d0348fd31645dba30e22b2fe37041f94a1
Mathias Staerk <mathsterk@redpill-linpro.com>
2021-02-15 22:13:55 +0100

    test

--- beater/varnishlogbeat.go
+++ beater/varnishlogbeat.go
@@ -178,6 +178,9 @@ func (vb *Varnishlogbeat) harvest() error {
 					txcounter[level][key] = 1
 				}
 				if _, ok := tx[tag]; ok {
+					count := strconv.FormatUint(txcounter[level][key], 10)
+					tx[tag].(common.MapStr)[level].(common.MapStr)[key].(common.MapStr)[count] = value
+					txcounter[level][key] += 1
 				} else {
 					tx[tag] = common.MapStr{level: common.MapStr{key: common.MapStr{"0": value}}}
 					// fmt.Printf("%d %s %s\n", txcounter[string(key)], key, value)
