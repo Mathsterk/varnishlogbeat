d549c5720b5fc3326889402d1f3c33b2e3df39ca
Mathias Staerk <mathsterk@redpill-linpro.com>
2021-02-15 17:11:43 +0100

    test

--- beater/varnishlogbeat.go
+++ beater/varnishlogbeat.go
@@ -174,35 +174,29 @@ func (vb *Varnishlogbeat) harvest() error {
 					tx[tag] = common.MapStr{level + "." + key + "." + "0": value}
 					// fmt.Printf("%d %s %s\n", txcounter[string(key)], key, value)
 				}
-			// case "VCL_acl":
-			// 	header := strings.SplitN(data, " ", 2)
-			// 	key := header[0]
-			// 	var value interface{}
-			// 	switch {
-			// 	case len(header) == 2:
-			// 		value = strings.TrimSpace(header[1])
-			// 	// if the header is too long, header and value might get truncated
-			// 	default:
-			// 		value = ""
-			// 	}
-			// 	if _, ok := tx[tag]; ok {
-			// 		count := strconv.FormatUint(txcounter[string(key)], 10)
-			// 		tx[tag].(common.MapStr)[key+"."+count] = value
-			// 		txcounter[string(key)] += 1
-			// 		// fmt.Printf("%d %s %s\n", txcounter[string(key)], key, value)
-			// 	} else {
-			// 		txcounter[string(key)] = 1
-			// 		tx[tag] = common.MapStr{key + "." + "0": value}
-			// 		// fmt.Printf("%d %s %s\n", txcounter[string(key)], key, value)
-			// 	}
 			case "VCL_call",
 				"VCL_use",
 				"VCL_return",
 				"VCL_acl":
+				header := strings.SplitN(data, " ", 2)
+				key := header[0]
+				var value interface{}
+				switch {
+				case len(header) == 2:
+					value = strings.TrimSpace(header[1])
+				// if the header is too long, header and value might get truncated
+				default:
+					value = "true"
+				}
 				if _, ok := tx[tag]; ok {
-					tx[tag].(common.StringSet).Add(data)
+					count := strconv.FormatUint(txcounter[string(key)], 10)
+					tx[tag].(common.MapStr)[key+"."+count] = value
+					txcounter[string(key)] += 1
+					// fmt.Printf("%d %s %s\n", txcounter[string(key)], key, value)
 				} else {
-					tx[tag] = common.MakeStringSet(data)
+					txcounter[string(key)] = 1
+					tx[tag] = common.MapStr{key + "." + "0": value}
+					// fmt.Printf("%d %s %s\n", txcounter[string(key)], key, value)
 				}
 			default:
 				tx[tag] = data
