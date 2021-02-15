f21f2688bb3d9b4648835a31c1a14dce2f3f2f8a
Mathias Staerk <mathsterk@redpill-linpro.com>
2021-02-15 17:16:43 +0100

    test

--- beater/varnishlogbeat.go
+++ beater/varnishlogbeat.go
@@ -174,10 +174,7 @@ func (vb *Varnishlogbeat) harvest() error {
 					tx[tag] = common.MapStr{level + "." + key + "." + "0": value}
 					// fmt.Printf("%d %s %s\n", txcounter[string(key)], key, value)
 				}
-			case "VCL_call",
-				"VCL_use",
-				"VCL_return",
-				"VCL_acl":
+			case "VCL_acl":
 				header := strings.SplitN(data, " ", 2)
 				key := header[0]
 				var value interface{}
@@ -196,6 +193,19 @@ func (vb *Varnishlogbeat) harvest() error {
 				} else {
 					txcounter[string(key)] = 1
 					tx[tag] = common.MapStr{key + "." + "0": value}
+					// fmt.Printf("%d %s %s\n", txcounter[string(key)], key, value)
+				}
+			case "VCL_call",
+				"VCL_return",
+				"VCL_use":
+				key := data
+				var value interface{}
+				value = "true"
+				if _, ok := tx[tag]; ok {
+					tx[tag].(common.MapStr)[key] = value
+					// fmt.Printf("%d %s %s\n", txcounter[string(key)], key, value)
+				} else {
+					tx[tag] = common.MapStr{key: value}
 					// fmt.Printf("%d %s %s\n", txcounter[string(key)], key, value)
 				}
 			default:
