802901eecf2f668577eb184d6c732dc63de49c65
Mathias Staerk <mathsterk@redpill-linpro.com>
2021-02-15 10:05:11 +0100

    test

--- beater/varnishlogbeat.go
+++ beater/varnishlogbeat.go
@@ -125,7 +125,11 @@ func (vb *Varnishlogbeat) harvest() error {
 				txcounter = nil
 				txcounter = make(map[string]uint64)
 
-			case "VCL_Log":
+			case "VCL_acl",
+				"VCL_call",
+				"VCL_use",
+				"VCL_return",
+				"VCL_Log":
 				header := strings.SplitN(data, ":", 2)
 				key := header[0]
 				var value interface{}
