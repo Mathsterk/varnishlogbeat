7d129177f592ebb3a4ed2489ba2343cf54f6d434
Mathias Staerk <mathsterk@redpill-linpro.com>
2021-02-15 22:46:59 +0100

    test

--- beater/varnishlogbeat.go
+++ beater/varnishlogbeat.go
@@ -182,20 +182,20 @@ func (vb *Varnishlogbeat) harvest() error {
 
 				if _, ok := tx[tag]; ok {
 					if _, oka := tx[tag].(common.MapStr)[level]; oka {
-						// 	if _, oku := tx[tag].(common.MapStr)[level].(common.MapStr)[key]; oku {
-						// 		if _, oku := tx[tag].(common.MapStr)[level].(common.MapStr)[key].(common.MapStr)[count]; oku {
-						// 			tx[tag].(common.MapStr)[level].(common.MapStr)[key].(common.MapStr)[count] = value
-						// 		} else {
-						// 			tx[tag].(common.MapStr)[level].(common.MapStr)[key] = common.MapStr{count: value}
-						// 		}
-						// 	} else {
-						// 		tx[tag].(common.MapStr)[level] = common.MapStr{key: common.MapStr{count: value}}
-						// 	}
+						if _, oku := tx[tag].(common.MapStr)[level].(common.MapStr)[key]; oku {
+							// 		if _, oku := tx[tag].(common.MapStr)[level].(common.MapStr)[key].(common.MapStr)[count]; oku {
+							// 			tx[tag].(common.MapStr)[level].(common.MapStr)[key].(common.MapStr)[count] = value
+							// 		} else {
+							// 			tx[tag].(common.MapStr)[level].(common.MapStr)[key] = common.MapStr{count: value}
+							// 		}
+						} else {
+							tx[tag].(common.MapStr)[level] = common.MapStr{key: common.MapStr{count: value}}
+						}
 					} else {
 						tx[tag] = common.MapStr{level: common.MapStr{key: common.MapStr{count: value}}}
 					}
 				} else {
-					tx[tag] = common.MapStr{level: common.MapStr{key: common.MapStr{"0": value}}}
+					tx[tag] = common.MapStr{level: common.MapStr{key: common.MapStr{count: value}}}
 				}
 
 				txcounter[level][key] += 1
