f561517388828ac6da852c1b5bef738c7f099814
Mathias Staerk <mathsterk@redpill-linpro.com>
2021-02-15 23:05:13 +0100

    test

--- beater/varnishlogbeat.go
+++ beater/varnishlogbeat.go
@@ -179,10 +179,10 @@ func (vb *Varnishlogbeat) harvest() error {
 				// }
 
 				if _, ok := txcounter[level][key]; ok {
-					txcounter[level][key] += 1
+					// txcounter[level][key] += 1
 				} else {
 					if _, oki := txcounter[level]; oki {
-						txcounter[level][key] = 1
+						// txcounter[level][key] = 1
 					} else {
 						txcounter[level] = map[string]uint64{}
 						txcounter[level][key] = 1
